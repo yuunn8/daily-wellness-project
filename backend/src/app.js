504700f9d6a2f6bc1ee4fb5efa58c9283561a9f0
yuunn8 <yujinshin0817@gmail.com>
2026-05-06 02:24:31 +0900

    fix: cors wildcard route

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,7 +24,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
