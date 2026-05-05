bfefdc17de6fbdec0f2137bd0562119884d86c37
yuunn8 <yujinshin0817@gmail.com>
2026-05-06 01:58:12 +0900

    replace bcrypt with bcryptjs

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const pool = require('../config/db');
 const env = require('../config/env');
 const { addHours } = require('../utils/date');
